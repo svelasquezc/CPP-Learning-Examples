e229d66ac6a1a58a30e855b7bdf967f83f4badd3
Steven Velásquez Chancí <svelasquezc@unal.edu.co>
2020-07-20 01:32:02 -0500

    Catch exception in loop, but it still tries to access permission-denied paths

--- src/RegexAndFilesystemExample.cpp
+++ src/RegexAndFilesystemExample.cpp
@@ -9,7 +9,7 @@
   
   This example finds a file by using r
 
- */
+*/
 
 namespace fs = std::filesystem;
 
@@ -44,10 +44,19 @@ int main(const int argc, const char* argv[]){
         std::cout << "Root directory: " << root << "\nFile to find name: " << file_to_find << std::endl;
 
         //TODO: Try to solve for root
-        try{
-            for(const auto& p :
-                    fs::recursive_directory_iterator(root,
-                                                     fs::directory_options::skip_permission_denied)){
+        //There is probably a bug because permission denied directories are still being accessed
+        //https://github.com/boostorg/filesystem/issues/112
+
+        //Using solution here, still trying to access non permitted directories
+        //https://stackoverflow.com/questions/58309449/c-how-to-use-stdfilesystemdirectory-options
+        
+        const fs::directory_options options = (
+                                               std::filesystem::directory_options::skip_permission_denied
+                                               );
+        
+        for(const auto& p :
+                fs::recursive_directory_iterator(root, options)){
+            try{
                 if(!p.is_directory()){
                     auto name = std::string(p.path().filename());
                 
@@ -72,9 +81,13 @@ int main(const int argc, const char* argv[]){
 
                     }
                 }
+            }catch(std::filesystem::filesystem_error &fse){
+                //We are going to ignore this...
+                //std::cout << "Caught fatal exception: "
+                //          << std::endl
+                //          << fse.what()
+                //          << std::endl;
             }
-        }catch(const std::exception& e){
-            std::cout << "Too many routes" <<std::endl;
         }
     }
 }
